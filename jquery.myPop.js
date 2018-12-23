72e3182db34ee1f23ecc166bef5194209971bece
zhalice2011 <zhalice2012@gmail.com>
2018-12-23 10:58:34 +0800

    feat: add some feature

--- jquery.myPop.js
+++ jquery.myPop.js
@@ -7,56 +7,58 @@
 
             var inputDom = $(this)
             var ph = inputDom.data('placeholder') || ''
+            var mode = inputDom.data('effect') || '1'
             var name = inputDom.attr('name')
             var value = inputDom.val()
 
+            var labelDom = null
             if (inputDom.parent().hasClass('form-item')) {
-                const lableNode = $(inputDom.siblings()[0])
-                if (value === '') {
-                    lableNode.css({
-                        top: inputDom.height() / 2 - 9,
-                        left: dom.offsetLeft + 5,
-                    })
-                } else {
-                    lableNode.css({
-                        top: -9,
-                        left: dom.offsetLeft + 5,
-                        fontSize: '12px',
-                    })
-                }
+                labelDom = inputDom.parent().find('.input-label')
+            } else {
+                inputDom.wrap('<div class="form-item"></div>')
+                var label =
+                '<label class="input-label" for=' + name + '>' + ph + '</label>'
+                labelDom = $(label)
+                inputDom.after(labelDom)
             }
 
-            inputDom.wrap('<div class="form-item"></div>')
-            var labelDom = null
-            var label =
-                '<label class="input-label" for=' + name + '>' + ph + '</label>'
-            labelDom = $(label)
             if (value === '') {
                 labelDom.css({
                     top: inputDom.height() / 2 - 9,
                     left: dom.offsetLeft + 5,
                 })
             } else {
-                labelDom.css({
-                    top: -9,
-                    left: dom.offsetLeft + 5,
-                    fontSize: '12px',
-                })
+                if (mode === '1') {
+                    labelDom.css({
+                        top: -9,
+                        left: dom.offsetLeft + 5,
+                        fontSize: '12px',
+                    })
+                } else {
+                    labelDom.css({
+                        fontSize: '14px',
+                        left: inputDom.offset().left + inputDom.width() - labelDom.width()
+                    })
+                }
             }
-
-            inputDom.after(labelDom)
-
             inputDom.focus(function() {
                 inputDom.addClass('input-text-focus')
                 inputDom.addClass(inputClass)
                 labelDom.addClass('input-label-focus')
                 labelDom.addClass(labelClass)
                 inputDom.parent().addClass('form-section-focus')
-                labelDom.css({
-                    top: -9
-                })
-            })
+                if (mode === '1') {
+                    labelDom.css({
+                        top: -9
+                    })
+                } else {
+                    labelDom.css({
+                        fontSize: '14px',
+                        left: inputDom.offset().left + inputDom.width() - labelDom.width()
+                    })
+                }
 
+            })
             inputDom.blur(function() {
                 inputDom.parent().removeClass('form-section-focus')
                 inputDom.removeClass('input-text-focus')
@@ -65,30 +67,55 @@
                 if (inputDom.val() === '') { //
                     labelDom.removeClass('input-label-focus')
                     labelDom.removeClass(labelClass)
-                    labelDom.css({
-                        top: inputDom.height() / 2 - 9,
-                    })
+                    if (mode === '1') {
+                        labelDom.css({
+                            top: inputDom.height() / 2 - 9,
+                        })
+                    } else {
+                        labelDom.css({
+                            left: dom.offsetLeft + 5,
+                        })
+                    }
                 } else {
-                    labelDom.css({
-                        top: -9,
-                    })
+                    if (mode === '1') {
+                        labelDom.css({
+                            top: -9,
+                        })
+                    } else {
+                        labelDom.css({
+                            fontSize: '14px',
+                            left: inputDom.offset().left + inputDom.width() - labelDom.width()
+                        })
+                    }
                 }
             })
-
             inputDom.bind("input propertychange",function(e){
                 if (e.target.value === '') {
                     labelDom.removeClass('input-label-focus')
                     labelDom.removeClass(labelClass)
-                    labelDom.css({
-                        top: inputDom.height() / 2 - 9,
-                    })
+                    if (mode === '1') {
+                        labelDom.css({
+                            top: inputDom.height() / 2 - 9,
+                        })
+                    } else {
+                        labelDom.css({
+                            left: dom.offsetLeft + 5,
+                        })
+                    }
+
                 } else {
                     labelDom.addClass('input-label-focus')
-                    labelDom.css({
-                        top: -9,
-                    })
+                    if (mode === '1') {
+                        labelDom.css({
+                            top: -9,
+                        })
+                    } else {
+                        labelDom.css({
+                            left: inputDom.offset().left + inputDom.width() - labelDom.width()
+                        })
+                    }
                 }
-         });
+            })
         })
     }
 })(jQuery)
